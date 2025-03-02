a164846b0d9005da6a4b649b1592beba9364546b
Abednego Tenge <abednegotenge180@gmail.com>
2025-03-02 18:16:58 +0000

    Adding request functions to test.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -8,10 +9,11 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class VideoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     views = db.Column(db.Integer, nullable=False)
     likes = db.Column(db.Integer, nullable=False)
@@ -21,10 +23,10 @@ class VideoModel(db.Model):
 
 
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
-video_put_args.add_argument('views', type=int, help="Views of the video", required=True)
-video_put_args.add_argument('likes', type=int, help="Likes on the video", required=True)
+video_post_args = reqparse.RequestParser()
+video_post_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_post_args.add_argument('views', type=int, help="Views of the video", required=True)
+video_post_args.add_argument('likes', type=int, help="Likes on the video", required=True)
 
 
 video_patch_args = reqparse.RequestParser()
@@ -51,10 +53,13 @@ class Video(Resource):
 
     @marshal_with(resource_fields)
     def put(self, video_id):
-        args = video_put_args.parse_args()
-        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-        if video:
-            abort(409, message='Video id taken!')
+        args = video_post_args.parse_args()
+        video = VideoModel.query.filter_by(id=video_id).first()
+        if not video:
+            abort(404, message="Video does not exist")
+        video.name = args['name']
+        video.views = args['views']
+        video.likes = args['likes']
         db.session.add(video)
         db.session.commit()
         return video, 201
@@ -80,13 +85,22 @@ class Video(Resource):
 
 
     def delete(self, video_id):
-        video = VideoModel.query.get(id=video_id)
+        video = VideoModel.query.filter_by(id=video_id).first()
         db.session.delete(video)
         db.session.commit()
         return '', 204
         
+class VideoList(Resource):
+    @marshal_with(resource_fields)
+    def post(self):
+        args = video_post_args.parse_args()
+        video = VideoModel(name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
 
 api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(VideoList, "/video")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,38 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-update = requests.get(BASE + 'video/2')
-print(update.json())
+def patch(video_id, data):
+    if not isinstance(data, dict):
+        raise TypeError('Input must be a dictionary')
+    response = requests.patch(BASE + f'video/{video_id}', data)
+    print(response.json())
+
+def get(video_id):
+    response = requests.get(BASE + f'video/{video_id}')
+    print(response.json())
+
+def post(data):
+    if not isinstance(data, dict):
+        raise TypeError('Input must be a dictionary')
+    response = requests.post(BASE + f'video', data)
+    print(response.json())
+
+def put(video_id, data):
+    if not isinstance(data, dict):
+        raise TypeError('Input must be a dictonary')
+    response = requests.put(BASE + f'video/{video_id}', data)
+    print(response.json())
+
+def delete(video_id):
+    response = requests.delete(BASE + f'video/{video_id}')
+    if response.status_code == 204:
+        print(f'Video with id {video_id} deleted')
+    else:
+        print(response.text)
+
+
+# patch(2, {'likes': 10, 'name': 'Tim', 'views': 1000})
+# get(4)
+# post({'likes': 200, 'name': 'Kingdom of Apes','views': 1000})
+# delete(3)
+
